91fb4ef99c1c0709f3c98842be8506507d56339c
NKGMadlife <1778139321@qq.com>
2021-04-27 09:51:25 +0800

    feat：接入ILRuntime1.6.7，规整项目目录

--- Unity/Assets/Hotfix/Demo/MapHelper.cs
+++ Unity/Assets/Hotfix/Demo/MapHelper.cs
@@ -1,5 +1,5 @@
 using System;
-
+using UnityEngine;
 
 namespace ET
 {
@@ -13,7 +13,6 @@ namespace ET
 
                 UnitComponent unitComponent = zoneScene.GetComponent<UnitComponent>();
                 unitComponent.MyUnit = unitComponent.Get(g2CEnterMap.UnitId);
-                
                 Game.EventSystem.Publish(new EventType.EnterMapFinish() {ZoneScene = zoneScene}).Coroutine();
             }
             catch (Exception e)
