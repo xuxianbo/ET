84eca70cc97506c8becd0e78a3680e4e18152935
NKGMadlife <1778139321@qq.com>
2021-04-28 21:01:40 +0800

    chorn：补充提交

--- Unity/Assets/Hotfix/Module/Message/IMessage.cs
+++ Unity/Assets/Hotfix/Module/Message/IMessage.cs
@@ -1,4 +1,4 @@
-﻿namespace ET
+﻿namespace ETHotfix
 {
     public interface IMessage
     {
--- Unity/Assets/Hotfix/Module/Message/IMessageDispatcher.cs
+++ Unity/Assets/Hotfix/Module/Message/IMessageDispatcher.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
 
-namespace ET
+namespace ETHotfix
 {
     public interface IMessageDispatcher
     {
