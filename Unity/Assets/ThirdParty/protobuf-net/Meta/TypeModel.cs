33d1cb9a4e22856d8da93a1a3a51827bd60b075a
NKGMadlife <1778139321@qq.com>
2021-05-01 18:08:23 +0800

    feat：LitJson和protobuf-net支持反序列化热更类型

--- Unity/Assets/ThirdParty/protobuf-net/Meta/TypeModel.cs
+++ Unity/Assets/ThirdParty/protobuf-net/Meta/TypeModel.cs
@@ -2,6 +2,11 @@
 using System.IO;
 
 using System.Collections;
+#if !SERVER
+
+using ILRuntime.Reflection;
+
+#endif
 #if FEAT_IKVM
 using Type = IKVM.Reflection.Type;
 using IKVM.Reflection;
@@ -851,7 +856,15 @@ namespace ProtoBuf.Meta
                 || !model.MapType(typeof(IEnumerable)).IsAssignableFrom(listType)) return null;
 			if (listType.FullName == "System.String") return null;
 #endif
-            
+#if !SERVER
+
+            if (listType is ILRuntimeWrapperType)
+            {
+                return ((ILRuntimeWrapperType)listType).CLRType.GenericArguments[0].Value.ReflectionType;
+            }
+
+#endif
+
             BasicList candidates = new BasicList();
 #if WINRT
             foreach (MethodInfo method in listType.GetRuntimeMethods())
