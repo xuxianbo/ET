5e0a81f18a9742895dc190097dc5a5b157a65fe4
wqaetly <1778139321@qq.com>
2021-08-18 22:23:04 +0800

    upd：跟进更新至https://github.com/egametang/ET/commit/9a065264c5e2cf618d3fbf77fc103d685ae9c14e

---
+++ Robot/App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robot.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Robot/Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robot.Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
