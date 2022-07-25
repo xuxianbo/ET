13086d913980c3ec6521bac1fe9db552b8c1f3e2
NKG丶MadLife <1778139321@qq.com>
2022-07-25 22:53:48 +0800

    fix：补充上传技能指示器

--- Unity/HuatuoData/init_local_il2cpp_data.bat
+++ Unity/HuatuoData/init_local_il2cpp_data.bat
@@ -15,7 +15,7 @@ git switch %IL2CPP_BRANCH%
 cd ..
 
 rem replace with right Unity Editor Install path
-set IL2CPP_PATH=D:\Study\Unity\UnityEditor\Unity2020.3.33f1\Editor\Data\il2cpp
+set IL2CPP_PATH=D:\Study\Unity\UnityEditor\2020.3.33f1c2\Editor\Data\il2cpp
 
 if not exist "%IL2CPP_PATH%" (
     echo "please set correct IL2CPP_PATH value"
